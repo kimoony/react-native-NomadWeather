1522d0ffac11be571098125d9e0ce0d6ccf5710a
Kim Hoon <71738968+kimoony@users.noreply.github.com>
2022-03-03 04:44:14 +0900

    weather features done

--- App.js
+++ App.js
@@ -2,9 +2,20 @@ import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Text, ScrollView, Dimensions, ActivityIndicator } from 'react-native';
 import * as Location from 'expo-location';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
 const API_KEY = "8b4400e8424eb03dac3f2ff4dc6ae6e1";
+const icons = {
+  Clouds: "weather-cloudy",
+  Clear: "weather-sunny",
+  Rain: "weather-pouring",
+  Snow: "weather-snowy",
+  Atmospheric: "weather-widy-variant",
+  Drizzle: "weather-rainy",
+  Thunderstorm: "weather-lightning-rainy",
+}
 
 export default function App() {
   const [city, setCity] = useState("Loading...");
@@ -22,7 +33,7 @@ export default function App() {
       { useGoogleMaps: false }
     );
     setCity(location[0].city)
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`);
     const json = await response.json();
     setDays(json.daily)
   }
@@ -40,11 +51,20 @@ export default function App() {
         pagingEnabled
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.weather}>
-        {days.length === 0 ? <View style={styles.day}>
-
-        </View> : <View style={styles.day}>
+        {days.length === 0 ? (<View style={styles.day}>
           <ActivityIndicator color="white" size="large" style={{ marginTop: 10 }} />
-        </View>}
+        </View>) : (
+          days.map((day, i) => (
+            <View key={i} style={styles.day}>
+              <View style={{ flexDirection: "row", width: "100%", justifyContent: "space-between", marginRight: "20%", }}>
+                <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+                <MaterialCommunityIcons name={icons[day.weather[0].main]} size={70} color="white" />
+              </View>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
       <StatusBar style="light" />
 
@@ -55,7 +75,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "blue",
+    backgroundColor: "#114A78",
   },
   city: {
     flex: 1,
@@ -65,21 +85,25 @@ const styles = StyleSheet.create({
   cityName: {
     fontSize: 50,
     fontWeight: "600",
-  },
-  weather: {
-
+    color: "#fff",
   },
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
+    paddingLeft: 30,
+    padding: 15,
   },
   temp: {
-    marginTop: 10,
-    fontSize: 180,
-    fontWeight: "600",
+    fontSize: 128,
+    fontWeight: "500",
+    color: "#fff",
   },
-  dscription: {
-    marginTop: -30,
-    fontSize: 60,
+  description: {
+    marginTop: -10,
+    fontSize: 30,
+    color: "#fff",
+  },
+  tinyText: {
+    fontSize: 20,
+    color: "#fff",
   }
 });
